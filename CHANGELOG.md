4ea6d954af51d48d96473980c31cca5424c357c3
Ahmad Awais <mrahmadawais@gmail.com>
2017-09-28 13:36:24 +0500

    VER: 0.0.7 — Improve PHP7 Compat — FIX Spacing 💯

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 #WPGitHubAPI
 
 ### Version 0.0.7
-- NEW: Transients below 172 characters
+- IMPROVE: PHP 7 compat — thanks to @[tfirdaus](https://github.com/tfirdaus) for PR #[1](https://github.com/ahmadawais/WPGitHubAPI/pull/1).
+- FIX: PHP CS Issues like weird spacing and tabs.
+- NEW: Transients below 172 characters.
 
 ### Version 0.0.6
 - NEW: Should there be `h1` with `h1=y|n`.
--- wpgithubapi.php
+++ wpgithubapi.php
@@ -5,7 +5,7 @@
  * Description: Shortcode to obtain resources from official GitHub API v3.
  * Author: mrahmadawais, WPTie
  * Author URI: http://AhmadAwais.com/
- * Version: 0.0.6
+ * Version: 0.0.7
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: https://github.com/ahmadawais/WPGitHubAPI/
@@ -18,22 +18,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-
 /**
  * Define global constants.
  *
  * @since 0.0.2
  */
 if ( ! defined( 'WGA_NAME' ) ) {
-    define( 'WGA_NAME', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
+	define( 'WGA_NAME', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
 }
 
-if ( ! defined('WGA_DIR' ) ) {
-    define( 'WGA_DIR', WP_PLUGIN_DIR . '/' . WGA_NAME );
+if ( ! defined( 'WGA_DIR' ) ) {
+	define( 'WGA_DIR', WP_PLUGIN_DIR . '/' . WGA_NAME );
 }
 
-if ( ! defined('WGA_URL' ) ) {
-    define( 'WGA_URL', WP_PLUGIN_URL . '/' . WGA_NAME );
+if ( ! defined( 'WGA_URL' ) ) {
+	define( 'WGA_URL', WP_PLUGIN_URL . '/' . WGA_NAME );
 }
 
 /**
@@ -42,7 +41,7 @@ if ( ! defined('WGA_URL' ) ) {
  * @since 1.0.0
  */
 if ( file_exists( WGA_DIR . '/class/class-wp-github-api-shortcodes.php' ) ) {
-    require_once( WGA_DIR . '/class/class-wp-github-api-shortcodes.php' );
+	require_once( WGA_DIR . '/class/class-wp-github-api-shortcodes.php' );
 }
 
 new WP_GitHub_API_Shortcodes();
